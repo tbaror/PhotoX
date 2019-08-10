e8f8e1f5daf05995e2c1d01e40f79610f60750c7
Tal Bar-Or <olsysystems@gmail.com>
2019-08-10 12:18:57 +0300

    add smooth scroll

--- script.js
+++ script.js
@@ -53,6 +53,25 @@ $(document).ready(function() {
   });
 });
 
+// Get the current year for the copyright
+$('#year').text(new Date().getFullYear());
+
+
+// Smooth Scrolling
+$("#main-nav a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      const hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+
+        window.location.hash = hash;
+      });
+    }
+  });
 
 
 
